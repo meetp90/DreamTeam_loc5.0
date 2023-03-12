467f5455990304ee3d551efbcbe36570ca905384
kenilshahh <kenilshah1505@gmail.com>
2023-03-12 10:33:51 +0530

    bass

--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,43 @@
-const path = require("path");
+/** @format */
 
+const path = require('path');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 module.exports = {
   compilers: {
     solc: {
       settings: {
         optimizer: {
           enabled: true,
-          runs: 200  
-        }
-      }
-    }
+          runs: 200,
+        },
+      },
+    },
   },
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
   networks: {
     develop: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 8545,
-      network_id: "*", // match any network
-      websockets: true
-    }
-  }
-  
+      network_id: '*', // match any network
+      websockets: true,
+    },
+    mumbai: {
+      provider: () =>
+        new HDWalletProvider(
+          '320c6ca026d3e4338a0569dedfa1c4faec7465235b32810c4e9f80f9969a296d',
+          'wss://app.zeeve.io/shared-api/poly/437e5102a5be36c3216c3da7a3bb7dedfb81282197494209'
+        ),
+      network_id: 80001
+      // gasPrice: 1000000,
+      // networkCheckTimeout: 100000000,
+      // timeoutBlocks: 200
+    },
+  },
+  // compilers: {
+  //   solc: {
+  //     version: "^0.8.0",
+  //   },
+  // },
 };
